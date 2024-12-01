a7628027748b9426737eddf1e2b4061c847975d8
fedinaimi <fedi.naimi@esprit.tn>
2024-12-01 23:05:58 +0100

    ddddddddddddd

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://theroomback.onrender.com',
-                changeOrigin: true, // Needed for CORS
-                secure: true, // Set this to false if your backend uses self-signed certificates
-            },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://theroomback.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
     },
+  },
 });
